34375bb1b145a79fe2685c8c063c0922dc864ff0
jahongireshonqulov <jahongireshonqulov17@gmail.com>
2026-05-19 15:52:25 +0500

    feat: Unit test made and tested for CommentModel

---
+++ test/features/comments/data/models/comment_model_test.dart
@@ -0,0 +1,36 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:tdd_example/features/comments/data/models/comment_model.dart';
+
+void main() {
+  group('CommentModel testing', () {
+    const tJson = {
+      'postId': 1,
+      'id': 42,
+      'name': 'John Doe',
+      'email': 'john@example.com',
+      'body': 'Hello world!',
+    };
+
+    const tModel = CommentModel(
+      postId: 1,
+      id: 42,
+      name: 'John Doe',
+      email: 'john@example.com',
+      body: 'Hello world!',
+    );
+
+    test('fromJson -Jsondan to\'g\'ri model yasaydi!', () {
+      final result = CommentModel.fromJson(tJson);
+      expect(result, equals(tModel));
+    });
+
+    test('DEFAULT qiymatlar - bo\'sh json bilan ishlaydi!', () {
+      final result = CommentModel.fromJson({});
+      expect(result.postId, equals(0));
+      expect(result.id, equals(0));
+      expect(result.email, equals(''));
+      expect(result.name, equals(''));
+      expect(result.body, equals(''));
+    });
+  });
+}
