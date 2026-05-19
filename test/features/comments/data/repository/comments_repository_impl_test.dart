bbcb05a08ab3e8b216346ace06d295fa224e7fbd
jahongireshonqulov <jahongireshonqulov17@gmail.com>
2026-05-19 17:02:05 +0500

    feat: Unit test made and tested for CommentsRepositoryImpl

---
+++ test/features/comments/data/repository/comments_repository_impl_test.dart
@@ -0,0 +1,45 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:tdd_example/features/comments/data/datasource/comments_datasource.dart';
+import 'package:tdd_example/features/comments/data/models/comment_model.dart';
+import 'package:tdd_example/features/comments/data/repository/comments_repository_impl.dart';
+
+class MockCommentsDatasource extends Mock implements CommentsDatasource {}
+
+void main() {
+  late MockCommentsDatasource mockDatasource;
+  late CommentsRepositoryImpl repository;
+
+  setUp(() {
+    mockDatasource = MockCommentsDatasource();
+    repository = CommentsRepositoryImpl(mockDatasource);
+  });
+
+  final tComments = [
+    CommentModel(
+      id: 1,
+      postId: 18,
+      name: 'Mark Jen',
+      email: 't@t.com',
+      body: 'Hey, flutter devs!',
+    ),
+  ];
+  const tDuration = Duration(hours: 1);
+
+  group('CommentsRepositoryImpl test', () {
+    test(
+      'getComments-datasource natijasini to\'g\'ridan to\'g\'ri qaytaradi!',
+      () async {
+        when(
+          () => mockDatasource.getComments(cacheDuration: tDuration),
+        ).thenAnswer((_) async => Right(tComments));
+        final result = await mockDatasource.getComments(cacheDuration: tDuration);
+        expect(result, equals(Right<dynamic, List<CommentModel>>(tComments)));
+        verify(
+          () => mockDatasource.getComments(cacheDuration: tDuration),
+        ).called(1);
+      },
+    );
+  });
+}
