97a81102ac92d7e2b6eac50fdcba06558e693924
jahongireshonqulov <jahongireshonqulov17@gmail.com>
2026-05-20 17:35:49 +0500

    feat: firebase integrated project and CD set up done

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tdd_example/core/di/di.dart';
 import 'package:tdd_example/core/router/routes.dart';
 import 'package:tdd_example/core/theme/theme.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   configureDependencies();
   runApp(const MyApp());
 }
